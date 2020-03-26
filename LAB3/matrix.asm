898639cea26676ac020558acdaaec801823dc27c
manturovDan <manturov.dan@mail.ru>
2020-03-26 19:40:15 +0300

    quite ready

--- LAB3/matrix.asm
+++ LAB3/matrix.asm
@@ -5,65 +5,51 @@
 n	db	5
 m	db	6
 nzcount	db	3
-chain	dw	0,0,300, 4,4,11, 5,2,-7
-matrix	dw	30 dup (?)
+chain	dw	1,2,300, 4,4,11, 5,2,-7
+matrix	dw	30 dup (0)
 	.code
 Start:	mov	ax, @data
 	mov	ds, ax
-	mov	si, offset matrix
-	xor	ax, ax
-	xor	bx, bx
-	mov	al, n
-	mov	bl, m
-	mul	bx
-	mov	bx, 0
 	mov	di, ax ;count of elements
 
-	;cmp	n, 0
-	;jle	er
-	;cmp	m, 0
-	;jle	er
-	cld
-	
-	xor	ecx, ecx
-	mov	cl, n ;rows
 	xor	ebx, ebx	
 
-	LROWS:
-		push	ecx
-		xor	ecx, ecx
-		
-		
-		mov	cl, m ;cols
-		LCOLS:
-			xor	ax, ax
-			mov	al, nzcount
-			mov	dl, 3
-			mul	dl
-			mov	dx, ax ;9
-			xor	ax, ax
+	mov	al, nzcount
+	mov	bl, 3
+	mul	al
+	mov	cx, ax ; end of triades array
+	shl cx, 1
 
-			WI:	cmp	ax, dx
-				jge	ROW
-				
-				push	ebx
-				
-				xor	ebx, ebx
-				mov	bx, [si + ax + 1]
+	xor	bx, bx
+	L: cmp bx, cx
+	jge	CE
+		mov	si, offset chain
+		mov ax, [si + bx] ;number of row
+		push cx
+		inc bx
+		inc bx
+		mov cx, [si + bx] ;number of column
+		inc	bx
+		inc bx
+		mov	dx, [si + bx]
+		inc bx
+		inc bx
 
-				cmp	bx, 				
+		mov si, offset matrix
+		mul	n
+		add	ax, cx
+		push bx
+		mov bx, ax
+		shl	bx, 1
+		mov [si + bx], dx
 
-				pop	ebx
-				
-				add	ax, 3				
-				jmp	WI
-			ROW: 	inc	ebx
-		loop LCOLS
-		pop	ecx
-		
-	loop LROWS
+		pop bx
+		pop cx
+		jmp	L
 
-	mov	ax, 4C00h
+
+
+CE:	mov	ax, 4C00h
 	int	21h
 ;;er:	mov	ax, 4C01h
 ;	jmp	ex
